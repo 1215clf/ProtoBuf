ca6df68a2715a1021ccc79213a94425d14f5d5d4
clf <clf@wifi.local>
2018-03-13 14:47:11 +0800

    Initial Commit

---
+++ ProtoBuf/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProtoBuf
+//
+//  Created by clf on 2018/3/13.
+//  Copyright © 2018年 com.clf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
